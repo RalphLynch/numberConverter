5e7e548e39fc41f2668efa5149f6b7aaa77ea224
06504 <ralphlynch1@protonmail.com>
2020-11-17 12:05:30 +0000

    Did some stuff

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,6 +18,14 @@ public class Main {
         return denary;
     }
 
+    public static String denaryToBinary (int denaryInput) {
+        String denary = Integer.toString(denaryInput);
+
+        for (int i = 0; i < denary.length(); i++) {
+            
+        }
+    }
+
     public static String binaryToHex (String binaryInput) {
         while (binaryInput.length() % 4 != 0) binaryInput = "0" + binaryInput;
 
@@ -49,32 +57,16 @@ public class Main {
         return hex;
     }
 
+    public static String hexToBinary (String hexInput) {
+        String binary = "";
+
+        for (int i = 0; i < hexInput.length(); i++)
+    }
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int selection = 0;
-
-        while (selection < 1 || selection > 6) {
-            System.out.println("Choose a number:");
-            System.out.println("1. Binary to Denary");
-            System.out.println("2. Denary to Binary");
-            System.out.println("3. Binary to Hex");
-            System.out.println("4. Hex to Binary");
-            System.out.println("5. Denary to Hex");
-            System.out.println("6. Hex to Denary");
-            System.out.println("Select an option");
-            selection = scan.nextInt();
-        }
+        String input = scan.nextLine();
 
 
-
-        if (selection == 1) {
-            System.out.println("Enter a binary number:");
-            String input = scan.nextLine();
-            System.out.println(binaryToDenary(input));
-        } else if (selection == 3) {
-            System.out.println("Enter a binary number:");
-            String input = scan.nextLine();
-            System.out.println(binaryToHex(input));
-        }
     }
 }
